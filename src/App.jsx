917eb5b8310abb88022539ab54bce61913a21262
yonixxxxx <yonixxxxx@example.com>
2026-08-19 06:02:22 +0300

    Регистрация

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registration from './components/registration/registration';
 import Login from './components/authorization/authorization';
 import Home from './components/Home/Home';
-import CreateProduct from './components/createProduct/CreateProduct'; // <--- ИМПОРТ НОВОЙ СТРАНИЦЫ
+import CreateProduct from './components/createProduct/CreateProduct'; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Главная страница (лендинг) */}
+      
         <Route path="/" element={<Home />} />
         
-        {/* Страница регистрации */}
+      
         <Route path="/register" element={<Registration />} />
         
-        {/* Страница входа */}
+      
         <Route path="/login" element={<Login />} />
         
-        {/* Страница создания товара */}
+      
         <Route path="/create-product" element={<CreateProduct />} />
       </Routes>
     </BrowserRouter>
